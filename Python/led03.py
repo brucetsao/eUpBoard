4e1463ff05259d4a00950574e11ecd3e00f1a24f
Bruce Tsao <prgbruce@gmail.com>
2016-10-26 01:08:22 +0800

    commit all

--- Python/led03.py
+++ Python/led03.py
@@ -1,4 +1,4 @@
 import RPi.GPIO as GPIO ## Import GPIO library
 GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
-GPIO.output(7,True) ## Turn on GPIO pin 
+GPIO.setup(21, GPIO.OUT) ## Setup GPIO Pin 21 to OUT
+GPIO.output(21,True) ## Turn on GPIO pin 
